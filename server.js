6cabb2827b6b4ccd71fed6ef7519a2ac40559664
Vahid Dejwakh <dejwakh@gmail.com>
2021-07-29 09:57:18 -0400

    reconfigured server to use OOP

--- server.js
+++ server.js
@@ -1,91 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const redis = require('redis');
 require ('dotenv').config();
+
+const ClientsManager = require('./lib/clientsManager');
+const Publisher = require('./lib/publisher');
+
 const app = express();
-
-const Registration = require('./lib/registration');
-const NotificationHandler = require('./lib/notification');
-const ClientManager = require('./lib/clientManager');
-const Client = require('./lib/client');
-
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
-const {PORT, REDIS_PORT, REDIS_HOST, JWT_KEY, SEC_PER_PULSE} = process.env;
+const {PORT, REDIS_PORT, REDIS_HOST, JWT_KEY, SEC_PER_PULSE, API_TOPICS} = process.env;
 
-const clients = new ClientManager();
+const clients = new ClientsManager(JWT_KEY, API_TOPICS);
+const publisher = new Publisher(REDIS_PORT, REDIS_HOST, SEC_PER_PULSE, clients.list);
+publisher.listenAndPublish();
 
-let pulse = parseInt(SEC_PER_PULSE, 10) * 1000;
-if (Number.isNaN(pulse) || pulse < 10000) pulse = 25000;
-// 10 seconds is min user can set
-// if blank or less than 10s, reset to 25s
+app.get('/stream/:topic/:id/:token', clients.stream.bind(clients));
 
-const redisSubscriber = redis.createClient(REDIS_PORT, REDIS_HOST);
+// for ALB health check
+app.get('/', (req, res) => res.send('Healthy'));
 
-redisSubscriber.on('connect', () => {
-  redisSubscriber.subscribe('redis'); // subscribe to redis topic (1 topic only)
-  console.log(`Connected to redis at ${REDIS_PORT}`);
-});
-
-redisSubscriber.on('message', (channel, message) => {
-  const data = JSON.parse(message);
-  console.log(`${new Date()} --> from redis: ${message}`);
-  clients.sendNewDataToAll(data.topic, `data: ${message}\n\n`);
-  if (data.topic !== 'all') clients.sendNewDataToAll('all', `data: ${message}\n\n`);
-});
-
-function verifyAndRegisterUser(topic, id, token, res) {
-  let newReg = new Registration(topic, id, token);
-  let notify = new NotificationHandler(res);
-
-  if (newReg.hasValidTopic()) {
-    try {
-      newReg.verifyJWT(JWT_KEY);
-      console.log(`Client ${id} verified.`)
-      return newReg.id;
-    } catch {
-      console.log(`Invalid token. Client ${id} cannot be connected.`)
-      notify.invalidToken();
-    }
-  } else {
-    console.log(`Invalid topic. Client ${id} cannot be connected.`)
-    notify.invalidTopic();
-  }
-  return false;
-}
-
-function httpStreamHandler(req, res, next) {
-  const {topic, id, token} = req.params;
-
-  let registrationId = verifyAndRegisterUser(topic, id, token, res);
-
-  if (registrationId) {
-    const newClient = new Client(registrationId, token);
-    clients.connectClient(newClient, topic, res);
-    clients.reportOnAll();
-
-    req.on('close', () => {
-      clients.disconnectClient(newClient, topic);
-      clients.reportOnAll();
-    })
-  }
-}
-
-// sending a heartbeat/pulse is necessary to regularly check on health of all connections
-// regularly send pulse to check connection
-setInterval(clients.pulseToAll.bind(clients), pulse);
-
-//routes
-
-app.get('/stream/:topic/:id/:token', httpStreamHandler);
-
-// updated for ALB health check
-app.get('/', (req, res) => {
-  res.send('Healthy');
-});
-
-app.listen(PORT, () => {
-  console.log(`listening at localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
